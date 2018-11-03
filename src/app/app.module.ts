1e43a98d1f6ca2244f61e7c48093581abe7b5d6e
Ravin Gunawardena <RavinSG@users.noreply.github.com>
2018-11-03 21:54:50 +0530

    Add dish class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
