37c4fc653af02a0ec7ad95774d9dbb4c0859ef3c
Ravin Gunawardena <RavinSG@users.noreply.github.com>
2018-11-04 10:15:21 +0530

    Add items to menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 
@@ -17,7 +18,8 @@ import { MenuComponent } from './menu/menu.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,7 +1,9 @@
 export class Dish {
+  id: string;
   name: string;
   image: string;
   category: string;
+  featured: boolean;
   label: string;
   price: string;
   description: string;
