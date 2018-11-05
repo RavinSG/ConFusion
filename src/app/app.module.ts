5af6bb0b2fd7caa4582b07444b27d3e3997a2e97
Ravin Gunawardena <RavinSG@users.noreply.github.com>
2018-11-05 22:05:23 +0530

    Fetch data by promotion service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,12 @@ import {MenuComponent} from './menu/menu.component';
 import {DishdetailComponent} from './dishdetail/dishdetail.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
 
 import {DishService} from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
 
@@ -44,7 +45,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatCardModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
